924b4db34a517c7b069db002b7f7f8568b8d16f3
Debojitmahapatra <debojitmahapatra19985@gmail.com>
2022-07-18 15:53:48 +0530

    Debojit

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -1,21 +1,70 @@
 const UrlModel = require("../Models/urlModels");
+const shortid = require("shortid");
+const validUrl = require("valid-url");
 
 
 const urlCreate = async function (req, res) {
+    const { longUrl } = req.body;
+  const baseUrl = "http://localhost:3000"
+
+  // Check base url
+  if (!validUrl.isUri(baseUrl)) {
+    return res.status(400).send({status:false,message:'Invalid base url'});
+  }
+
+  // Create url code
+  const urlCode = shortid.generate();
+
+  // Check long url
+  if (validUrl.isUri(longUrl)) {
     try {
-        res.setHeader('Access-Control-Allow-Origin','*')
-        let data = req.body;
-        
-        let createUrl = await UrlModel.create(data);
-      
-        res.status(201).send({ status: true, data: createUrl })
+      let url = await UrlModel.findOne({ longUrl }).select({longUrl:1,shortUrl:1,urlCode:1,_id:0});
+
+      if (url) {
+       return res.status(200).send({status:true,data:url});
+
+      } else {
+        const shortUrl = baseUrl + '/' + urlCode;
+
+        deRurl ={
+            longUrl:longUrl,
+            shortUrl:shortUrl,
+            urlCode:urlCode        
+        };
+
+        let newUrl=await UrlModel.create(deRurl);
+       let finelResult=await UrlModel.findById(newUrl._id).select({longUrl:1,shortUrl:1,urlCode:1,_id:0})
+        res.status(200).send({status:true,data:finelResult});
+      }
     } catch (err) {
-        res.status(500).send({ error: err.message })
+      console.error(err);
+      res.status(500).send('Server error');
     }
+  } else {
+    res.status(401).send({status:false,message:'Invalid long url'});
+  }
+};
 
+function x(data) {
+    if (!data || data == null || data === undefined || data.trim() == 0) return false;
+    return true
+}
+const getUrl= async function (req, res){
+    try{
+        let url=req.params.urlCode;
+        if (!x(url)) return res.status(400).send({status:false,message:"enter valid urlcode"})
+        let findUrl=await UrlModel.findOne({urlCode:url});
+        if(findUrl){
+            return res.status(200).redirect(findUrl.longUrl)//.send({status:true,message:`this url redirect to=> ${findUrl.longUrl}`})
+        }else{
+            return res.status(404).send({status:false,message:'No url found'})
+        }    
+    }catch(err){
+        return res.status(500).json('Server error');
+    }
 }
 
 
-module.exports = {
-    urlCreate
-}
+
+module.exports = { urlCreate,getUrl}
+
--- src/route/route.js
+++ src/route/route.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const  { urlCreate,getUrl}= require("../Controllers/urlController");
 
-
+router.post("/url/shorten",urlCreate)
+router.get("/:urlCode",getUrl)
 
 module.exports = router
