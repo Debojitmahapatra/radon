79e063b41acb9c1bf0100fc135c81b769cbe97ba
Debojitmahapatra <debojitmahapatra19985@gmail.com>
2022-07-19 11:24:28 +0530

    debojit

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -1,10 +1,14 @@
 const UrlModel = require("../Models/urlModels");
 const shortid = require("shortid");
 const validUrl = require("valid-url");
+const reurl=/(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
 
 
 const urlCreate = async function (req, res) {
     const { longUrl } = req.body;
+    if (Object.keys(req.body).length == 0) { return res.status(400).send({ status: false, msg: "Bad request- Please enter details in the request Body " }) }
+    if (!x(longUrl)) { return res.status(400).send({ status: false, msg: "Please enter your longUrl" }) }
+
   const baseUrl = "http://localhost:3000"
 
   // Check base url
@@ -16,7 +20,7 @@ const urlCreate = async function (req, res) {
   const urlCode = shortid.generate();
 
   // Check long url
-  if (validUrl.isUri(longUrl)) {
+  if (validUrl.isUri(longUrl)&&reurl.test(longUrl)) {
     try {
       let url = await UrlModel.findOne({ longUrl }).select({longUrl:1,shortUrl:1,urlCode:1,_id:0});
 
@@ -41,7 +45,7 @@ const urlCreate = async function (req, res) {
       res.status(500).send('Server error');
     }
   } else {
-    res.status(401).send({status:false,message:'Invalid long url'});
+    res.status(400).send({status:false,message:'Invalid long url'});
   }
 };
 
@@ -53,14 +57,15 @@ const getUrl= async function (req, res){
     try{
         let url=req.params.urlCode;
         if (!x(url)) return res.status(400).send({status:false,message:"enter valid urlcode"})
+        if (!(url.length >= 7 && url.length <= 14)) return res.status(400).send({ status: false, message: "Enter a valid urlcode" })
         let findUrl=await UrlModel.findOne({urlCode:url});
         if(findUrl){
-            return res.status(200).redirect(findUrl.longUrl)//.send({status:true,message:`this url redirect to=> ${findUrl.longUrl}`})
+            return res.status(302).redirect(findUrl.longUrl)//.send({status:true,message:`this url redirect to=> ${findUrl.longUrl}`})
         }else{
             return res.status(404).send({status:false,message:'No url found'})
         }    
     }catch(err){
-        return res.status(500).json('Server error');
+        return res.status(500).send('Server error');
     }
 }
 
